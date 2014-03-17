b1a88a71dd5a41314f5367bd5b9640a84b7df2e1
Jerry <Jeremias.Boos@gmx.de>
2014-03-17 19:40:14 +0100

    Inital ServerEntityManager !Warnung! Noch nicht gestestet!

---
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/entity/EntityType.java
@@ -0,0 +1,12 @@
+package de.hochschuletrier.gdw.ws1314.entity;
+
+/**
+ * Created by jerry on 17.03.14.
+ */
+public enum EntityType {
+    Knight,
+    Hunter,
+    Tank,
+    Ei,
+    Projectil
+}
