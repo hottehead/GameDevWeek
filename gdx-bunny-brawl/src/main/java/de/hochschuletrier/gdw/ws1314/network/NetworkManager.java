b43480c59adf642b9d55759f6ae993a7d078a48f
Stefan Bodenschatz <stefan_bodenschatz@hotmail.com>
2014-03-17 20:28:45 +0100

    Client- und Server-State im NetworkManager.

--- gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/network/NetworkManager.java
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/network/NetworkManager.java
@@ -1,12 +1,62 @@
 package de.hochschuletrier.gdw.ws1314.network;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.hochschuletrier.gdw.commons.netcode.NetConnection;
+import de.hochschuletrier.gdw.commons.netcode.NetReception;
+import de.hochschuletrier.gdw.commons.netcode.datagram.INetDatagramFactory;
+
 public class NetworkManager {
+    private static final Logger logger = LoggerFactory.getLogger(NetworkManager.class);
 	private NetworkManager instance = new NetworkManager();
+
+	private NetConnection clientConnection=null;
+	private ArrayList<NetConnection> serverConnections=null;
+	private NetReception serverReception=null;
+	private INetDatagramFactory datagramFactory;//TODO Implement
+	
 	private NetworkManager(){}
 	public NetworkManager getInstance(){
 		return instance;
 	}
 	
+	public void connect(String ip, int port){
+		if(isClient()){
+			logger.warn("Ignoring new connect command because we are already connected.");
+		}
+		try {
+			clientConnection=new NetConnection(ip, port, datagramFactory);
+		} catch (IOException e) {
+			logger.error("Can't connect.",e);
+		}
+	}
+	
+	public void listen(String ip, int port, int maxConnections){
+		if(isServer()){
+			logger.warn("Ignoring new listen command because we are already a server.");
+		}
+		serverConnections=new ArrayList<NetConnection>();
+		try {
+			serverReception = new NetReception(ip, port, maxConnections, datagramFactory);
+		} catch (IOException e) {
+			logger.error("Can't listen for connections.", e);
+			serverConnections=null;
+			serverReception=null;
+		}
+	}
+	
+	public boolean isServer() {
+		return serverConnections!=null && serverReception!=null;
+	}
+
+	public boolean isClient() {
+		return clientConnection!=null && clientConnection.isConnected();
+	}
+
 	public void sendEntityEvent(long id, int eventPlayerIntention){
 		//TODO: Implement
 	}
@@ -28,6 +78,10 @@ public class NetworkManager {
 	}
 	
 	public void removeChatListener(ChatListener listener){
+		//TODO: Implement
+	}
+	
+	public void receiveChat(String sender,String text){
 		//TODO: Implement
 	}
 }
