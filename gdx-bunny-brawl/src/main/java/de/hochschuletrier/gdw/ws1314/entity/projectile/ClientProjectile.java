ef3a6040dae1e78bf4c48d5316322e3bb7195b19
Sonic <Sonic@home.de>
2014-03-18 14:00:49 +0100

    Projectile Classes added

---
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/entity/projectile/ClientProjectile.java
@@ -0,0 +1,47 @@
+package de.hochschuletrier.gdw.ws1314.entity.projectile;
+
+import com.badlogic.gdx.math.Vector2;
+
+import de.hochschuletrier.gdw.ws1314.entity.ClientEntity;
+import de.hochschuletrier.gdw.ws1314.entity.EntityState;
+
+/**
+ * 
+ * @author Sonic
+ *
+ */
+
+public class ClientProjectile extends ClientEntity {
+
+	public ClientProjectile(Vector2 position, long id) {
+		super(position, id);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void enable() {
+	}
+
+	@Override
+	public void disable() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public void update(int delta) {
+	}
+
+	@Override
+	public void render() {
+	}
+
+	@Override
+	public void setEntityState(EntityState state) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
