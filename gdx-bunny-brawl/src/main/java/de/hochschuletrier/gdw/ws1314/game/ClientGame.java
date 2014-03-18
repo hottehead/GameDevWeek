dc9330e9a8df96edc800fb17942f9ca25b726fc1
Jerry <Jeremias.Boos@gmx.de>
2014-03-18 14:51:25 +0100

    Client Game PlayerIntention sends

--- gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/game/ClientGame.java
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/game/ClientGame.java
@@ -3,9 +3,13 @@ package de.hochschuletrier.gdw.ws1314.game;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
+import de.hochschuletrier.gdw.commons.gdx.input.InputInterceptor;
+import de.hochschuletrier.gdw.commons.gdx.state.transition.SplitHorizontalTransition;
 import de.hochschuletrier.gdw.ws1314.Main;
 import de.hochschuletrier.gdw.ws1314.entity.ClientEntityManager;
+import de.hochschuletrier.gdw.ws1314.input.PlayerIntention;
 import de.hochschuletrier.gdw.ws1314.network.NetworkManager;
+import de.hochschuletrier.gdw.ws1314.states.GameStates;
 
 /**
  * Created by Jerry on 18.03.14.
@@ -26,7 +30,6 @@ public class ClientGame {
 
     public void render() {
 
-
     }
 
     public void update(float delta) {
@@ -35,15 +38,57 @@ public class ClientGame {
 
     public boolean keyDown(int keycode) {
 
-        if(keycode == Input.Keys.A)
+        switch(keycode)
         {
-
+            case(Input.Keys.A):{
+                netManager.sendAction(PlayerIntention.MOVE_TOGGLE_LEFT);
+                break;
+            }
+            case(Input.Keys.S):{
+                netManager.sendAction(PlayerIntention.MOVE_TOGGLE_DOWN);
+                break;
+            }
+            case(Input.Keys.D):{
+                netManager.sendAction(PlayerIntention.MOVE_TOGGLE_RIGHT);
+                break;
+            }
+            case(Input.Keys.W):{
+                netManager.sendAction(PlayerIntention.MOVE_TOGGLE_UP);
+                break;
+            }
+            case(Input.Keys.SPACE):{
+                netManager.sendAction(PlayerIntention.ATTACK_1);
+                break;
+            }
+            case(Input.Keys.E):{
+                netManager.sendAction(PlayerIntention.DROP_EGG);
+                break;
+            }
         }
 
         return false;
     }
 
     public boolean keyUp(int keycode) {
+        switch(keycode)
+        {
+            case(Input.Keys.A):{
+                netManager.sendAction(PlayerIntention.MOVE_TOGGLE_LEFT);
+                break;
+            }
+            case(Input.Keys.S):{
+                netManager.sendAction(PlayerIntention.MOVE_TOGGLE_DOWN);
+                break;
+            }
+            case(Input.Keys.D):{
+                netManager.sendAction(PlayerIntention.MOVE_TOGGLE_RIGHT);
+                break;
+            }
+            case(Input.Keys.W):{
+                netManager.sendAction(PlayerIntention.MOVE_TOGGLE_UP);
+                break;
+            }
+        }
         return false;
     }
 
