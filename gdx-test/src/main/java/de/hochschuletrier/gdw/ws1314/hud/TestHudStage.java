911c8ed862e18c4098234a9ccebc08790f5b8c34
Hati Eth <hati.eth@gmail.com>
2014-03-18 17:36:33 +0100

    text decorator position

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ws1314/hud/TestHudStage.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ws1314/hud/TestHudStage.java
@@ -9,8 +9,10 @@ import com.badlogic.gdx.math.MathUtils;
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 import de.hochschuletrier.gdw.ws1314.hud.elements.BarBackgroundDecoration;
 import de.hochschuletrier.gdw.ws1314.hud.elements.BarFrontDecorator;
+import de.hochschuletrier.gdw.ws1314.hud.elements.BarTextDecorator;
 import de.hochschuletrier.gdw.ws1314.hud.elements.MinMaxValue;
 import de.hochschuletrier.gdw.ws1314.hud.elements.NinePatchSettings;
+import de.hochschuletrier.gdw.ws1314.hud.elements.StaticTextElement;
 import de.hochschuletrier.gdw.ws1314.hud.elements.VisualBar;
 
 public class TestHudStage extends AutoResizeStage {
@@ -27,19 +29,20 @@ public class TestHudStage extends AutoResizeStage {
 	@Override
 	public void init(AssetManagerX assetManager) {
 		Texture barTex = assetManager.getTexture("debugBar");
-		Texture backBarTex = assetManager.getTexture("debugClass");
+		Texture backBarTex = assetManager.getTexture("debugTooltip");
 		Texture frontBarTex = assetManager.getTexture("debugBarDecorNine");
 		
 		
 		
 		healthBar = new MinMaxValue(0, 100, -1);
-		VisualBar healthBarVisual =  new VisualBar(barTex, 0, 0, 300, 40, healthBar);
+		VisualBar healthBarVisual =  new VisualBar(barTex, 30, 30, 300, 40, healthBar);
 		
 		
 		BarBackgroundDecoration backgroundHealth = new BarBackgroundDecoration(healthBarVisual, backBarTex);
 //		BarFrontDecorator frontBar = new BarFrontDecorator(test, frontBarTex);
+		visualBar = new BarTextDecorator(backgroundHealth, new StaticTextElement(assetManager.getFont("verdana", 14), "100", 100, 0));
+		visualBar = new BarFrontDecorator(visualBar, frontBarTex, new NinePatchSettings(1, 2, 2, 1));
 		
-		visualBar = new BarFrontDecorator(backgroundHealth, frontBarTex, new NinePatchSettings(1, 2, 2, 1));
 		
 		healthBar.setValue(100);
 	}
--- gdx-test/src/main/java/de/hochschuletrier/gdw/ws1314/hud/elements/BarTextDecorator.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ws1314/hud/elements/BarTextDecorator.java
@@ -3,13 +3,16 @@ package de.hochschuletrier.gdw.ws1314.hud.elements;
 
 public class BarTextDecorator extends BarDecorator {
 
-	public BarTextDecorator(VisualBar decoratedBarBar, VisualElement decoration) {
-		super(decoratedBarBar, decoration);
+	public BarTextDecorator(VisualBar decoratedBar, VisualElement decoration) {
+		super(decoratedBar, decoration);
+		decoration.positionX = decoration.positionX + decoratedBar.positionX;
+		decoration.positionY = decoration.positionY + decoratedBar.positionY;
 	}
 
 	@Override
 	public void draw() {
-		
+		super.drawBar();
+		super.drawDecoration();
 	}
 
 	
