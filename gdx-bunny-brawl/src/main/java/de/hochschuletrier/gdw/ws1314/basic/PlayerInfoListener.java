077f7de2c38f209361cac43dfe6fed493a1a0374
Sonic <Sonic@home.de>
2014-03-18 16:27:08 +0100

    PlayerInfo removed

--- gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/basic/PlayerInfoListener.java
+++
@@ -1,9 +0,0 @@
-package de.hochschuletrier.gdw.ws1314.basic;
-
-
-public interface PlayerInfoListener {
-	public void teamChanged(PlayerInfo source);
-	public void readyStateChanged(PlayerInfo source);
-	public void characterClassChanged(PlayerInfo source);
-	public void nameChanged(PlayerInfo source);
-}
