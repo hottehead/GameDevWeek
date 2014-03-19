70372d350ae19363638c5798486a7dae0d42e171
Gimmillaro Fabio <Gimmillaro Fabio@GimmillaroFabio.home>
2014-03-19 14:22:40 +0100

    LevelLoad Aktualisiert

--- gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/game/ServerGame.java
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/game/ServerGame.java
@@ -127,6 +127,7 @@ public class ServerGame {
 				int y = layerObject.getY();
 
 				boolean b = l.getBooleanProperty("solid", false);
+				System.out.println(b);
 				if (b) {
 					Primitive p = layerObject.getPrimitive();
 					if (p == Primitive.POINT) {
