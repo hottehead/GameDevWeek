825ab1d93e6cd9ef60518adb6a0dc042a39e2e33
Namerian <patrickmonville@hotmail.com>
2014-03-19 14:57:14 +0100

    arbeit an der kollisions-kontrolle der Angriffe

--- gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/entity/projectile/ServerSwordAttack.java
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/entity/projectile/ServerSwordAttack.java
@@ -6,19 +6,50 @@
 
 package de.hochschuletrier.gdw.ws1314.entity.projectile;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.Manifold;
+import de.hochschuletrier.gdw.commons.gdx.physix.PhysixBody;
+import de.hochschuletrier.gdw.commons.gdx.physix.PhysixBodyDef;
+import de.hochschuletrier.gdw.commons.gdx.physix.PhysixFixtureDef;
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixManager;
 import de.hochschuletrier.gdw.ws1314.entity.EntityType;
 import static de.hochschuletrier.gdw.ws1314.entity.EntityType.SwordAttack;
 import de.hochschuletrier.gdw.ws1314.entity.ServerEntity;
+import de.hochschuletrier.gdw.ws1314.entity.ServerEntityManager;
+import de.hochschuletrier.gdw.ws1314.entity.player.ServerPlayer;
 
 /**
  *
  * @author Patrick
  */
 public class ServerSwordAttack extends ServerEntity {
+    
+    //========================================
+    // CONSTANTS
+    private static final float totalLifetime = 1500.0f;
+    
+    //========================================
+    // VARIABLES
+    private final long sourceID;
+    private final Vector2 originPosition;
+    private final Vector2 facingDirection;
+    
+    private float lifetime;
+    
+    //========================================
+    public ServerSwordAttack(long sourceID) {
+        super();
+        
+        this.sourceID = sourceID;
+        
+        ServerPlayer player = (ServerPlayer)ServerEntityManager.getInstance().getEntityById(sourceID);
+        
+        this.originPosition = player.getPosition();
+        this.facingDirection = player.getFacingDirection().getDirectionVector();
+    }
 
     @Override
     public void enable() {
@@ -38,6 +69,11 @@ public class ServerSwordAttack extends ServerEntity {
 
     @Override
     public void update(float deltaTime) {
+        lifetime += deltaTime;
+        
+        if(lifetime > totalLifetime) {
+            ServerEntityManager.getInstance().removeEntity(this);
+        }
     }
 
     @Override
@@ -47,6 +83,12 @@ public class ServerSwordAttack extends ServerEntity {
 
     @Override
     public void initPhysics(PhysixManager manager) {
+        PhysixBody body = new PhysixBodyDef(BodyDef.BodyType.StaticBody, manager).position(this.originPosition).fixedRotation(true).create();
+            body.createFixture(new PhysixFixtureDef(manager).density(0.5f).friction(0.0f).restitution(0.0f).shapeCircle(30).sensor(true));
+            body.setGravityScale(0);
+            body.addContactListener(this);
+            
+            setPhysicsBody(body);
     }
 
     @Override
