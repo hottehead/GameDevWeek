8300657dce3120d9780b68a9822c72d28779919f
rftpool13 <rftpool13@rft1312.rz.fh-trier.de>
2014-03-19 23:00:33 +0100

    moved LevelList and LevelLsitElement to the right package

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ws1314/hud/TestHudStage.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ws1314/hud/TestHudStage.java
@@ -14,6 +14,7 @@ import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 import de.hochschuletrier.gdw.commons.gdx.utils.DrawUtil;
 import de.hochschuletrier.gdw.ws1314.Main;
 import de.hochschuletrier.gdw.ws1314.hud.elements.HealthBar;
+import de.hochschuletrier.gdw.ws1314.hud.elements.LevelList;
 import de.hochschuletrier.gdw.ws1314.hud.elements.base.BoxOffsetDecorator;
 import de.hochschuletrier.gdw.ws1314.hud.elements.base.StaticTextElement;
 import de.hochschuletrier.gdw.ws1314.hud.elements.base.VisualBox;
