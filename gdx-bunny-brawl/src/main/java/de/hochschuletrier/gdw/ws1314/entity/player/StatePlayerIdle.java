fc7102e22e13fc6f1c50ce42dcaa9e7fbac9a8d7
ElFapo <el_fapo@posteo.ru>
2014-03-19 16:34:51 +0100

    # Added author javadoc stuff to my files

--- gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/entity/player/StatePlayerIdle.java
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/entity/player/StatePlayerIdle.java
@@ -3,6 +3,11 @@ package de.hochschuletrier.gdw.ws1314.entity.player;
 import de.hochschuletrier.gdw.ws1314.state.IStateListener;
 import de.hochschuletrier.gdw.ws1314.state.State;
 
+/**
+ * 
+ * @author ElFapo
+ *
+ */
 public class StatePlayerIdle extends State
 {
 
