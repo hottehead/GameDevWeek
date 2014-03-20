1cc24ee8b05c132a53550b5d034ae684aa40d9a2
Michael Ochmann <miko007@me.com>
2014-03-20 20:13:50 +0100

    cleaning merge

--- gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/states/MainMenuState.java
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/states/MainMenuState.java
@@ -2,12 +2,14 @@ package de.hochschuletrier.gdw.ws1314.states;
 
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import de.hochschuletrier.gdw.commons.gdx.assets.AnimationExtended;
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 import de.hochschuletrier.gdw.commons.gdx.input.InputInterceptor;
 import de.hochschuletrier.gdw.commons.gdx.state.GameState;
 import de.hochschuletrier.gdw.commons.gdx.state.transition.SplitHorizontalTransition;
+import de.hochschuletrier.gdw.commons.gdx.utils.DrawUtil;
 import de.hochschuletrier.gdw.ws1314.Main;
 import de.hochschuletrier.gdw.ws1314.hud.MainMenuStage;
 import de.hochschuletrier.gdw.ws1314.shaders.DemoShader;
@@ -58,73 +60,6 @@ public class MainMenuState extends GameState implements InputProcessor {
 		stage.init(assetManager);
 	}
 
-<<<<<<< HEAD
-    @Override
-    public void render() {
-		stage.render();
-    }
-
-    @Override
-    public void onEnter() {
-        inputProcessor.setActive(false);
-        
-        if (this.music.isMusicPlaying())
-        	this.music.deMute();
-        else
-        	this.music.play("music-lobby-loop");
-    }
-
-    @Override
-    public void onLeave() {
-    	this.music.mute();
-        inputProcessor.setActive(false);
-        
-    }
-
-    @Override
-    public void dispose() {
-    }
-
-    @Override
-    public boolean keyDown(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return true;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int amount) {
-        return false;
-    }
-=======
 	@Override
 	public void render() {
 		TextureRegion keyFrame = walking.getKeyFrame(stateTime);
@@ -206,5 +141,4 @@ public class MainMenuState extends GameState implements InputProcessor {
 	public boolean scrolled(int amount) {
 		return false;
 	}
->>>>>>> sound
 }
