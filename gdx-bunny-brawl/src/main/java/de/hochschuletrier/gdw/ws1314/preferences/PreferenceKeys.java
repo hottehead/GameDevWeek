b39549fb160c2e43be1b4b5551845abfce18637c
Michael Ochmann <miko007@me.com>
2014-03-21 23:18:52 +0100

    added screen resolution to preferenceKeys

--- gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/preferences/PreferenceKeys.java
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/preferences/PreferenceKeys.java
@@ -1,7 +1,14 @@
 package de.hochschuletrier.gdw.ws1314.preferences;
 
+/**
+ * List of existing keys for preferences to load or to save
+ * 
+ * @author MikO
+ */
 public enum PreferenceKeys {
 	volumeMusic,
 	volumeSound,
-	playerName
+	playerName,
+	gameWindowHeight,
+	gameWindowWidth
 }
