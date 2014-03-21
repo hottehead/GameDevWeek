f0e060f91563d893926f72f18263103dd45981ce
DerjenigeWelche <masterfg@gmx.net>
2014-03-21 17:32:53 +0100

    WasserKollision

--- gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/game/LevelLoader.java
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/game/LevelLoader.java
@@ -242,11 +242,12 @@ public class LevelLoader {
 		x += width / 2;
 		y += height / 2;
 
-        if(properties != null)
+        if(properties == null)
         {
-            properties.setFloat("width",width);
-            properties.setFloat("height",height);
+        	 properties.setFloat("width",width);
+             properties.setFloat("height",height);
         }
+       
         Zone zone;
         ServerEntity entity = null;
 		switch (type) {
