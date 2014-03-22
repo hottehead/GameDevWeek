d2b4d75d6f24d44d5c329cf6830981de1c4eb528
DerjenigeWelche <masterfg@gmx.net>
2014-03-22 19:26:35 +0100

    Kontaktmiene optimiert

--- gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/entity/levelObjects/ServerContactMine.java
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/entity/levelObjects/ServerContactMine.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.Fixture;
 
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixBody;
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixBodyDef;
@@ -12,6 +13,7 @@ import de.hochschuletrier.gdw.commons.gdx.physix.PhysixManager;
 import de.hochschuletrier.gdw.ws1314.entity.EntityStates;
 import de.hochschuletrier.gdw.ws1314.entity.EntityType;
 import de.hochschuletrier.gdw.ws1314.entity.ServerEntity;
+import de.hochschuletrier.gdw.ws1314.entity.player.ServerPlayer;
 
 /**
  * 
@@ -21,11 +23,16 @@ import de.hochschuletrier.gdw.ws1314.entity.ServerEntity;
 public class ServerContactMine extends ServerLevelObject {
 	private final float DURATION_TILL_EXPLOSION_MAX = 3.0f;
 	private final float DURATION_TILL_EXPLOSION_MIN = 1.0f;
-
+	
+	private float originRadius = 2.0f;
+	private final float EXPLOSION_RADIUS = 200.0f;
 	private final float DURATION_EXPLOSION = 1.0f;
 	private boolean isActive = false;
 	private float timer;
 	private long sourceID;
+	
+	private Fixture fixture1;
+	private Fixture fixture2;
 
 	public ServerContactMine() {
 		super();
@@ -44,16 +51,34 @@ public class ServerContactMine extends ServerLevelObject {
 
 	@Override
 	public void beginContact(Contact contact) {
+		
 		ServerEntity otherEntity = this.identifyContactFixtures(contact);
-
-		switch (otherEntity.getEntityType()) {
-		case Hunter:
-		case Knight:
-		case Tank:
-		case Noob:
-		case HayBale:
-			this.isActive = true;
-
+		Fixture f = this.getCollidingFixture(contact);
+		
+		if(f == fixture2){
+			switch (otherEntity.getEntityType()) {
+			case Hunter:
+			case Knight:
+			case Tank:
+			case Noob:
+			case HayBale:
+				ServerPlayer player = (ServerPlayer) otherEntity;
+			
+				break;
+			}
+		}else if(f == fixture1){
+			switch(otherEntity.getEntityType()){
+				case Hunter:
+				case Knight:
+				case Tank:
+				case Noob:
+				case HayBale:
+					ServerPlayer player = (ServerPlayer) otherEntity;
+					this.isActive = true;
+					break;
+				default:
+					break;
+			}
 		}
 	}
 
@@ -69,11 +94,15 @@ public class ServerContactMine extends ServerLevelObject {
 			timer -= deltaTime;
 			System.out.println("Time left: " + timer);
 			if (timer <= 0) {
-				System.out.println(" server mine exploding");
 				isActive = false;
 				entityState = EntityStates.EXPLODING;
 				timer = MathUtils.random(DURATION_TILL_EXPLOSION_MIN, DURATION_TILL_EXPLOSION_MAX);
-				this.physicsBody.getBody().getFixtureList().get(1);
+				if(originRadius <= EXPLOSION_RADIUS){
+					originRadius *= 2;
+					this.physicsBody.getBody().getFixtureList().get(1).setSensor(false);
+				}
+			}else{
+				this.physicsBody.getBody().getFixtureList().get(1).setSensor(true);
 			}
 		} else {
 			entityState = EntityStates.NONE;
@@ -95,10 +124,14 @@ public class ServerContactMine extends ServerLevelObject {
 		body.createFixture(new PhysixFixtureDef(manager).density(0.5f)
 				.friction(0.0f).sensor(true).restitution(0.0f).shapeBox(10, 10));
 		body.createFixture(new PhysixFixtureDef(manager).density(0.5f)
-				.friction(0.0f).sensor(true).restitution(0.0f).shapeCircle(2));
+				.friction(0.0f).sensor(true).restitution(0.0f).shapeCircle(EXPLOSION_RADIUS));
 		body.setGravityScale(0);
 		body.addContactListener(this);
 		setPhysicsBody(body);
+		
+		fixture1 = this.physicsBody.getBody().getFixtureList().get(0);
+		fixture2 = this.physicsBody.getBody().getFixtureList().get(1);
+		
 	}
 
 }
