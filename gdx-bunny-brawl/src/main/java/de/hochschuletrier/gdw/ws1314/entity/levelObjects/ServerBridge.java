ba60ba7260183878e1afb6c560a10da29ca5ccc1
Jerry <Jeremias.Boos@gmx.de>
2014-03-22 16:29:04 +0100

    Jetzt sollten alle Events drin sein

--- gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/entity/levelObjects/ServerBridge.java
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/entity/levelObjects/ServerBridge.java
@@ -11,6 +11,8 @@ import de.hochschuletrier.gdw.commons.gdx.physix.PhysixBodyDef;
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixFixtureDef;
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixManager;
 import de.hochschuletrier.gdw.ws1314.entity.EntityType;
+import de.hochschuletrier.gdw.ws1314.entity.EventType;
+import de.hochschuletrier.gdw.ws1314.network.NetworkManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -123,6 +125,11 @@ public class ServerBridge extends ServerLevelObject
 		return isVisible;
 	}
 	public void setVisiblity(boolean b){
+		if(isVisible)	{
+			NetworkManager.getInstance().sendEntityEvent(getID(), EventType.BRIDGE_IN);
+		} else{
+			NetworkManager.getInstance().sendEntityEvent(getID(), EventType.BRIDGE_OUT);
+		}
 		isVisible = b;
 	}
 
