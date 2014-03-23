a994d7b50253e83a36bb17d4ce527149c08b2a21
rftpool13 <rftpool13@rft1312.rz.fh-trier.de>
2014-03-23 13:29:59 +0100

    added right graphic for exit button

--- gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/hud/MainMenuStage.java
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/hud/MainMenuStage.java
@@ -3,8 +3,6 @@ package de.hochschuletrier.gdw.ws1314.hud;
 import org.lwjgl.opengl.GL11;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
@@ -14,7 +12,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.esotericsoftware.tablelayout.BaseTableLayout.Debug;
 
@@ -23,7 +20,6 @@ import de.hochschuletrier.gdw.commons.gdx.utils.DrawUtil;
 import de.hochschuletrier.gdw.ws1314.Main;
 import de.hochschuletrier.gdw.ws1314.hud.elements.LevelList;
 import de.hochschuletrier.gdw.ws1314.hud.elements.ListElement;
-import de.hochschuletrier.gdw.ws1314.preferences.PreferenceKeys;
 
 public class MainMenuStage extends AutoResizeStage {
 	
@@ -36,19 +32,11 @@ public class MainMenuStage extends AutoResizeStage {
 	private Table uiTable;
 	
 	//buttons
-//<<<<<<< HEAD
 	private ImageButton playServer;
 	private ImageButton gameBrowser;
 	private ImageButton options; 
 	private ImageButton credits;
 	private ImageButton exit;
-//=======
-//	private TextButton playServer;
-//	private TextButton gameBrowser;
-//	private TextButton options; 
-//	private TextButton credits;
-//	private TextButton exit;
-//>>>>>>> branch 'master' of https://github.com/hottehead/GameDevWeek.git
 	
 	//test
 	TextButton startServerAndPlay;
@@ -85,7 +73,6 @@ public class MainMenuStage extends AutoResizeStage {
 		Table tmpTable = new Table(); 
 		uiTable.add(tmpTable).pad(20);
 		
-//<<<<<<< HEAD
 		TextureRegion texture = new TextureRegion(assetManager.getTexture("menuButtonPlayClient"));
 		ImageButtonStyle style = new ImageButtonStyle(defaultSkin.get(ButtonStyle.class));
 		style.imageUp = new TextureRegionDrawable(texture);
@@ -106,18 +93,10 @@ public class MainMenuStage extends AutoResizeStage {
 		style.imageUp = new TextureRegionDrawable(texture);
 		credits = new ImageButton(style);
 		
-		//FIXME: change texture from help to exit
-		texture = new TextureRegion(assetManager.getTexture("menuButtonHelp"));
+		texture = new TextureRegion(assetManager.getTexture("menuButtonExit"));
 		style = new ImageButtonStyle(defaultSkin.get(ButtonStyle.class));
 		style.imageUp = new TextureRegionDrawable(texture);
 		exit = new ImageButton(style);
-//=======
-//		gameBrowser = new TextButton("Spielen als Client", defaultSkin);
-//		playServer = new TextButton("Spielen als Server", defaultSkin);
-//		options = new TextButton("Optionen", defaultSkin);
-//		credits = new TextButton("Credits", defaultSkin);
-//		exit = new TextButton("Beenden", defaultSkin);
-//>>>>>>> branch 'master' of https://github.com/hottehead/GameDevWeek.git
 		
 		tmpTable.add(gameBrowser).pad(5).prefSize(50);
 		tmpTable.add(playServer).pad(5).prefSize(50);
@@ -161,13 +140,8 @@ public class MainMenuStage extends AutoResizeStage {
 			uiTable.setScale(this.xScale, this.yScale);
 	}
 	
-//<<<<<<< HEAD
 	public ImageButton getGameBrowserButton() {
 		return gameBrowser;
-//=======
-//	public TextButton getGameBrowserButton() {
-//		return gameBrowser;
-//>>>>>>> branch 'master' of https://github.com/hottehead/GameDevWeek.git
 	}
 	
 	public ImageButton getPlayServerButton() {
