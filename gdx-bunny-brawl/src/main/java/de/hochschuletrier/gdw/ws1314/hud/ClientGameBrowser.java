dba847a519edf8643c9cc0d70b3a30c8d79462ec
Torsten Scholer <hotteport@online.de>
2014-03-23 12:27:35 +0100

    game browser ,playernamen

--- gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/hud/ClientGameBrowser.java
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/hud/ClientGameBrowser.java
@@ -1,10 +1,12 @@
 package de.hochschuletrier.gdw.ws1314.hud;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -15,13 +17,16 @@ import com.esotericsoftware.tablelayout.BaseTableLayout.Debug;
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 import de.hochschuletrier.gdw.commons.gdx.state.ScreenListener;
 import de.hochschuletrier.gdw.ws1314.Main;
+import de.hochschuletrier.gdw.ws1314.preferences.PreferenceKeys;
 import de.hochschuletrier.gdw.ws1314.states.ChooseClassState;
 
-public class ClientGameBrowser extends AutoResizeStage   {
+public class ClientGameBrowser extends AutoResizeStage {
 	private Logger logger = LoggerFactory.getLogger(ClientGameBrowser.class);
 
 	TextField serverIP;
 	TextField serverPort;
+	TextField playerName;
+	Label nameLabel;
 	Label IpLabel, PortLabel;
 	Button join;
 	private Skin defaultSkin;
@@ -37,6 +42,20 @@ public class ClientGameBrowser extends AutoResizeStage   {
 		join = new Button(new Label("Verbinden", defaultSkin), defaultSkin, "default");
 		serverIP = new TextField("127.0.0.1", defaultSkin);
 		serverPort = new TextField("54293", defaultSkin);
+		playerName = new TextField("Funny Bunny", defaultSkin);
+		playerName.addListener(new InputListener() {
+			@Override
+			public boolean keyUp(InputEvent event, int keycode) {
+				if (keycode == Keys.ENTER) {
+					Main.getInstance().gamePreferences.putString(PreferenceKeys.playerName, playerName.getText());
+					return true;
+				
+				}
+				return false;
+			}
+		});
+		root.add(playerName);
+		root.row();
 		root.add(serverIP);
 		root.add(serverPort);
 		root.row();
@@ -63,7 +82,7 @@ public class ClientGameBrowser extends AutoResizeStage   {
 		act();
 		draw();
 	}
-	
+
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
