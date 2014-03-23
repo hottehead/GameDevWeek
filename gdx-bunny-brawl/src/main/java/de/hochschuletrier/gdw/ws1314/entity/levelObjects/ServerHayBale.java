753fd1fd30683f614a5fc11b3ebfb5659b30b6b6
Namerian <patrickmonville@hotmail.com>
2014-03-23 14:38:39 +0100

    arbeit am heuball

--- gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/entity/levelObjects/ServerHayBale.java
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/entity/levelObjects/ServerHayBale.java
@@ -48,6 +48,7 @@ public class ServerHayBale extends ServerLevelObject
 	
 	private boolean collWaterUpperLeft, collWaterUpperRight, collWaterLowerLeft, collWaterLowerRight;
 	private boolean collAbyssUpperLeft, collAbyssUpperRight, collAbyssLowerLeft, collAbyssLowerRight;
+	private boolean collHayBaleUpperLeft, collHayBaleUpperRight, collHayBaleLowerLeft, collHayBaleLowerRight;
 	
 	private HashMap<Long, ServerPlayer> playersOnHayBale;
 	private int isOnBridge;
@@ -126,6 +127,9 @@ public class ServerHayBale extends ServerLevelObject
 		        case AbyssZone:
 		            this.collAbyssUpperLeft = true;
 		            break;
+		        case HayBale:
+		            this.collHayBaleUpperLeft = true;
+		            break;
 		        default:
                     break;
 		    }
@@ -136,6 +140,9 @@ public class ServerHayBale extends ServerLevelObject
                     break;
                 case AbyssZone:
                     this.collAbyssUpperRight = true;
+                    break;
+                case HayBale:
+                    this.collHayBaleUpperRight = true;
                     break;
                 default:
                     break;
@@ -148,6 +155,9 @@ public class ServerHayBale extends ServerLevelObject
                 case AbyssZone:
                     this.collAbyssLowerLeft = true;
                     break;
+                case HayBale:
+                    this.collHayBaleLowerLeft = true;
+                    break;
                 default:
                     break;
             }
@@ -158,6 +168,10 @@ public class ServerHayBale extends ServerLevelObject
                     break;
                 case AbyssZone:
                     this.collAbyssLowerRight = true;
+                    break;
+                case HayBale:
+                    this.collHayBaleLowerRight = true;
+                    break;
                 default:
                     break;
             }
@@ -206,6 +220,9 @@ public class ServerHayBale extends ServerLevelObject
                 case AbyssZone:
                     this.collAbyssUpperLeft = false;
                     break;
+                case HayBale:
+                    this.collHayBaleUpperLeft = false;
+                    break;
                 default:
                     break;
             }
@@ -216,6 +233,9 @@ public class ServerHayBale extends ServerLevelObject
                     break;
                 case AbyssZone:
                     this.collAbyssUpperRight = false;
+                    break;
+                case HayBale:
+                    this.collHayBaleUpperRight = false;
                     break;
                 default:
                     break;
@@ -228,6 +248,9 @@ public class ServerHayBale extends ServerLevelObject
                 case AbyssZone:
                     this.collAbyssLowerLeft = false;
                     break;
+                case HayBale:
+                    this.collHayBaleLowerLeft = false;
+                    break;
                 default:
                     break;
             }
@@ -238,6 +261,10 @@ public class ServerHayBale extends ServerLevelObject
                     break;
                 case AbyssZone:
                     this.collAbyssLowerRight = false;
+                    break;
+                case HayBale:
+                    this.collHayBaleLowerRight = false;
+                    break;
                 default:
                     break;
             }
@@ -265,27 +292,19 @@ public class ServerHayBale extends ServerLevelObject
             .position(new Vector2(posX, posY))
             .fixedRotation(true).create();
 
-        
+        //main fixture
     	body.createFixture(new PhysixFixtureDef(manager)
 		    .density(0.5f)
 		    .friction(0.0f)
 		    .restitution(0.0f)
 		    .shapeBox(50,50));
     	
-    	//main fixture
-    	body.createFixture(new PhysixFixtureDef(manager)
-            .density(0.5f)
-            .friction(0.0f)
-            .restitution(0.0f)
-            .shapeBox(1, 1, new Vector2(-25, -25), 0)
-            .sensor(true));
-    	
     	//upper left coll check
     	body.createFixture(new PhysixFixtureDef(manager)
             .density(0.5f)
             .friction(0.0f)
             .restitution(0.0f)
-            .shapeBox(1, 1, new Vector2(25, -25), 0)
+            .shapeBox(1, 1, new Vector2(-23, -23), 0)
             .sensor(true));
     	
     	//upper right coll check
@@ -293,7 +312,7 @@ public class ServerHayBale extends ServerLevelObject
             .density(0.5f)
             .friction(0.0f)
             .restitution(0.0f)
-            .shapeBox(1, 1, new Vector2(-25, -25), 0)
+            .shapeBox(1, 1, new Vector2(23, -23), 0)
             .sensor(true));
     	
     	//lower left coll check
@@ -301,7 +320,7 @@ public class ServerHayBale extends ServerLevelObject
             .density(0.5f)
             .friction(0.0f)
             .restitution(0.0f)
-            .shapeBox(1, 1, new Vector2(-25, 25), 0)
+            .shapeBox(1, 1, new Vector2(-23, 23), 0)
             .sensor(true));
     	
     	//lower right coll check
@@ -309,7 +328,7 @@ public class ServerHayBale extends ServerLevelObject
             .density(0.5f)
             .friction(0.0f)
             .restitution(0.0f)
-            .shapeBox(1, 1, new Vector2(25, 25), 0)
+            .shapeBox(1, 1, new Vector2(23, 23), 0)
             .sensor(true));
         
         body.setGravityScale(0);
@@ -345,26 +364,32 @@ public class ServerHayBale extends ServerLevelObject
                 this.reset();
             }
         } else if(collWaterUpperLeft && collWaterUpperRight && collWaterLowerLeft && collWaterLowerRight) {
-            this.physicsBody.setLinearVelocity(new Vector2());
-            this.fixtureMain.setSensor(true);
-            this.acrossable = true;
-            this.setEntityState(EntityStates.WET);
-            NetworkManager.getInstance().sendEntityEvent(getID(), EventType.DRWONING);
-            speed = 0;
-            
-            Iterator<Long> keySetIterator = this.playersOnHayBale.keySet().iterator();
-            
-            while(keySetIterator.hasNext()) {
-                Long key = keySetIterator.next();
-                ServerPlayer player = this.playersOnHayBale.get(key);
-                player.setPlayerIsOnBridge();
+            if(!collHayBaleLowerLeft && !collHayBaleLowerRight && !collHayBaleUpperLeft && !collHayBaleUpperRight) {
+                this.physicsBody.setLinearVelocity(new Vector2());
+                this.fixtureMain.setSensor(true);
+                this.acrossable = true;
+                this.setEntityState(EntityStates.WET);
+                NetworkManager.getInstance().sendEntityEvent(getID(), EventType.DRWONING);
+                speed = 0;
+                
+                Iterator<Long> keySetIterator = this.playersOnHayBale.keySet().iterator();
+                
+                while(keySetIterator.hasNext()) {
+                    Long key = keySetIterator.next();
+                    ServerPlayer player = this.playersOnHayBale.get(key);
+                    player.setPlayerIsOnBridge();
+                    }
             }
-            
         } else if(collAbyssUpperLeft && collAbyssUpperRight && collAbyssLowerLeft && collAbyssLowerRight) {
             if(this.isOnBridge == 0) {
                 this.reset();
             }
         }
+    }
+    
+    public void reset() {
+        super.reset();
+        this.fixtureMain.setSensor(false);
     }
 
 }
