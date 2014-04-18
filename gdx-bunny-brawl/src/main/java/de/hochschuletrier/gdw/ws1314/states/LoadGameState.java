1745931f3b29ca31702ff16daba9d9dbba2f8352
Torsten Scholer <hotteport@online.de>
2014-04-18 21:24:11 +0200

    Revert "Splashscreen (unfertig)"
    This reverts commit bda32a7c7a7eb914e578fb722fae54010e43bbe9.

--- gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/states/LoadGameState.java
+++ gdx-bunny-brawl/src/main/java/de/hochschuletrier/gdw/ws1314/states/LoadGameState.java
@@ -2,8 +2,6 @@ package de.hochschuletrier.gdw.ws1314.states;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 import de.hochschuletrier.gdw.commons.gdx.state.GameState;
@@ -12,47 +10,40 @@ import de.hochschuletrier.gdw.ws1314.Main;
 
 public class LoadGameState extends GameState {
 
-	private boolean isDone;
-	TextureRegion splashscreen;
-	
-	@Override
-	public void init(AssetManagerX assetManager) {
-		super.init(assetManager);
-		splashscreen = new TextureRegion(new Texture(
-				Gdx.files.internal("data/images/ladescreen.png")));
-		
-	}
+    private boolean isDone;
 
-	@Override
-	public void render() {
-		DrawUtil.fillRect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(),
-				Color.BLACK);
-		DrawUtil.batch.draw(splashscreen, 0, 0);
-		float drawWidth = Gdx.graphics.getWidth() - 100.0f;
-		DrawUtil.fillRect(50, Gdx.graphics.getHeight() / 2 - 25,
-				(int) (drawWidth * assetManager.getProgress()), 50, Color.GREEN);
-		DrawUtil.drawRect(50, Gdx.graphics.getHeight() / 2 - 25, drawWidth, 50,
-				Color.GREEN);
-	}
+    @Override
+    public void init(AssetManagerX assetManager) {
+        super.init(assetManager);
+    }
 
-	@Override
-	public void update(float delta) {
-		if (isDone) {
-			return;
-		}
+    @Override
+    public void render() {
+        DrawUtil.fillRect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), Color.BLACK);
 
-		assetManager.update();
+        float drawWidth = Gdx.graphics.getWidth() - 100.0f;
+        DrawUtil.fillRect(50, Gdx.graphics.getHeight() / 2 - 25, (int) (drawWidth * assetManager.getProgress()), 50, Color.GREEN);
+        DrawUtil.drawRect(50, Gdx.graphics.getHeight() / 2 - 25, drawWidth, 50, Color.GREEN);
+    }
 
-		if (assetManager.getProgress() == 1) {
-			// VSync was only disabled to speed up loading
-			Gdx.graphics.setVSync(true);
+    @Override
+    public void update(float delta) {
+        if (isDone) {
+            return;
+        }
 
-			Main.getInstance().onLoadComplete();
-			isDone = true;
-		}
-	}
+        assetManager.update();
 
-	@Override
-	public void dispose() {
-	}
+        if (assetManager.getProgress() == 1) {
+            // VSync was only disabled to speed up loading
+            Gdx.graphics.setVSync(true);
+
+            Main.getInstance().onLoadComplete();
+            isDone = true;
+        }
+    }
+
+    @Override
+    public void dispose() {
+    }
 }
